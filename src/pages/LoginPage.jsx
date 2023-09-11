568287360a6ff551abeb219e723f56f36f461a22
Jessica Spivak <jls103193@yahoo.com>
2023-09-11 12:29:27 -0400

    trying to figure out login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,76 @@
-export default function LoginPage() {
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const API_URL = 'http://localhost:4500/api'
+
+export default function LoginPage({setToken}) {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+  
+    const navigate = useNavigate()
+    async function submitForm(e) {
+      e.preventDefault()
+      const login = async () => {
+        try {
+            const response = await fetch(`${API_URL}/users/login`, 
+            { 
+                method: "POST", 
+                headers: { 
+                    "Content-Type": "application/json" 
+                }, 
+                body: JSON.stringify({ 
+                        username, 
+                        password 
+                }) 
+            })
+            const result = await response.json();
+            if (result.success) {
+              const { token } = result.data
+              localStorage.setItem('token', token);
+              setToken(token)
+              navigate('/')
+            } else {
+              setErrorMessage(result.error.message)
+            }   
+            console.log(result)
+            
+        } catch (err) {
+            console.error(err)
+        }          
+    }
+      login()
+    }
+    
     return (
-        <h1>Login Page</h1>
+      <div>
+       
+        <h1>Log In</h1>
+        <form onSubmit={submitForm}>
+          <label htmlFor="username">Username: </label>
+          <input
+            value={username} 
+            type="username"
+            id="username"
+            onChange={(e) => {
+              setErrorMessage('');
+              setUsername(e.target.value)
+            }} 
+          />
+          <br></br>
+          <label htmlFor="password">Password: </label>
+          <input
+            value={password}
+            type="password"
+            id="password"
+            onChange={(e) => {
+              setErrorMessage('');
+              setPassword(e.target.value)
+            }}
+          />
+          <p>{errorMessage}</p>
+          <button type="submit">Log In</button>
+        </form>
+      </div>
     )
-}
+  }

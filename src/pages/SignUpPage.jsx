8b224d9d0e5318b5be713f345443dca4aa66da50
Jessica Spivak <jls103193@yahoo.com>
2023-09-25 14:00:27 -0400

    still can't figure out how to show the error for a username already existing and forwards alphabetical order

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,22 +1,52 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { validPassword } from '../components/regex.js'
+// import { getUserByUsername } from '../API/index.js'
 
 const API_URL = 'http://localhost:4500/api'
 // const API_URL = 'https://freecats.onrender.com/api'
 
 export default function SignUpPage({setToken}) {
+    // const [users, setUsers] = useState([])
     const [name, setName] = useState('')
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    const [errorMessage, setErrorMessage] = useState(false)
-
+    const [pwdErrorMessage, setPwdErrorMessage] = useState(false)
+    const [usrErrorMessage, setUsrErrorMessage] = useState(false)
+    
+    // async function getUsers(username) {
+    //     try {
+    //         const response = await fetch(`${API_URL}/users/register`, {
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             }
+    //         })
+    //         const result= await response.json();
+    //         // const users = result;
+    //         setUsers(result)
+    //         console.log(result, "result")
+    //         console.log(username, "username")
+    //         console.log(users, "result.username")
+    //         if (username === result.username) {
+    //             return true;
+    //         } else {
+    //             return false;
+    //         }  
+    //     } catch(err) {
+    //         console.error(err);
+    //     }
+    // }
     const navigate = useNavigate()
     async function submitForm(e) {
         e.preventDefault()
         if(!validPassword.test(password)) {
-            setErrorMessage(true);
+            setPwdErrorMessage(true);
+        //if username already exists, error
+        // } else if (getUserByUsername(username)) {
+        //     setUsrErrorMessage(true)
         } else { 
+            
             const signUp = async () => {
                 try {
                     const response = await fetch(`${API_URL}/users/register`, 
@@ -32,8 +62,9 @@ export default function SignUpPage({setToken}) {
                         }) 
                     })
                     const result = await response.json();
+                   
                     const { token } = result
-                    console.log(result)
+                    // console.log(result)
                     localStorage.setItem('token', token);
                     setToken(token)
                     navigate('/')
@@ -55,7 +86,7 @@ export default function SignUpPage({setToken}) {
                 type="name"
                 id="name"
                 onChange={(e) => {
-                    setErrorMessage(false);
+                    // setErrorMessage(false);
                     setName(e.target.value)
                 }} 
             />
@@ -65,7 +96,7 @@ export default function SignUpPage({setToken}) {
                 type="username"
                 id="username"
                 onChange={(e) => {
-                    setErrorMessage(false);
+                    setUsrErrorMessage(false);
                     setUsername(e.target.value)
                 }} 
             />
@@ -76,7 +107,7 @@ export default function SignUpPage({setToken}) {
                 type="password"
                 id="password"
                 onChange={(e) => {
-                    setErrorMessage(false);
+                    setPwdErrorMessage(false);
                     setPassword(e.target.value)
                 }}
             />
@@ -84,8 +115,8 @@ export default function SignUpPage({setToken}) {
             <br></br>
             <button type="submit">Sign Up</button>
         </form>
-        {errorMessage && <p>Your password must contain at least one uppercase letter, one lowercase letter, one number and one special character.</p>}
-        
+        {pwdErrorMessage && <p>Your password must contain at least one uppercase letter, one lowercase letter, one number and one special character.</p>}
+        {usrErrorMessage && <p>That username is already taken</p>}
         </div>
     )
 }

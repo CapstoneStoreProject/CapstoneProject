57172e9f89d775806581eca3719ffd2575e9c9bd
Jessica Spivak <jls103193@yahoo.com>
2023-10-29 10:40:11 -0400

    delete from cart now navigates user to cats page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,13 +2,16 @@ import { Link } from 'react-router-dom'
 import CatCard from "../components/CatCard"
 import { fetchCatById } from "../API"
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Cart({token, cat, id, cart, setCart}) {
     console.log(cart)
+    const navigate = useNavigate()
     // const [catCart, setCatCart] = useState([])
     async function deleteCat(id) {
         // const Cat = await fetchCatById(id, token)
         setCart(cart.filter(cat => cat.id !== id))
+        navigate('/')
     }
 
     return (
